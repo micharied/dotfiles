d6aec5566fe848b120647af7e582d94ffeda62c6
Micha Riedlinger <kontakt@micha-riedlinger.de>
2025-04-23 14:45:12 +0200

    Revert "activate tlp for hopefully better battery life"
    This reverts commit 44590415715b63e6a8ad6059187e193ded4b8664.

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,6 @@
   ];
 
   services.udev.packages = with pkgs; [ gnome-settings-daemon ];
-  services.tlp.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
