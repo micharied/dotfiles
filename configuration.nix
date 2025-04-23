44590415715b63e6a8ad6059187e193ded4b8664
Micha Riedlinger <kontakt@micha-riedlinger.de>
2025-04-23 14:39:54 +0200

    activate tlp for hopefully better battery life

--- configuration.nix
+++ configuration.nix
@@ -113,6 +113,7 @@
   ];
 
   services.udev.packages = with pkgs; [ gnome-settings-daemon ];
+  services.tlp.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
