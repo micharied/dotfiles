7a0adaa2955ab1cf1c9e595025e6bac6859bf158
Micha Riedlinger <kontakt@micha-riedlinger.de>
2025-11-18 11:38:49 +0100

    fix git warnings

--- programs/git.nix
+++ programs/git.nix
@@ -2,29 +2,20 @@
   config = {
     programs.git = {
       enable = true;
-      userEmail = "kontakt@micha-riedlinger.de";
-      userName = "Micha Riedlinger";
       includes = [{
-        condition = "gitdir:~/dev/activegroup/";
+        condition = "gitdir:~/dev/ag/";
         contents = { user.email = "micha.riedlinger@active-group.de"; };
       }];
       ignores = [
         ".envrc"
         ".direnv/"
-
         ".calva"
       ];
-      extraConfig = {
+      settings = {
+        user.email = "kontakt@micha-riedlinger.de";
+        user.name = "Micha Riedlinger";
         init.defaultBranch = "main";
         checkout.defaultRemote = "origin";
-        # url = {
-        #   "https://github.com/" = {insteadOf = "gh:";};
-        #   "git@github.com:" = {insteadOf = "gs:";};
-        #   "git@github.com:haglobah/" = {insteadOf = "my:";};
-        #   "https://gitlab.com/" = {insteadOf = "gl:";};
-        #   "ssh://git@gitlab.active-group.de:1022/ag/" = {insteadOf = "ag:";};
-        #   "git@github.com:active-group/" = {insteadOf = "agh:";};
-        # };
       };
     };
   };
