bd00246bfd55e65cfcee79d7a4c301cf93ad0c96
Micha Riedlinger <kontakt@micha-riedlinger.de>
2025-12-02 08:22:09 +0100

    pin hm to 25.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
